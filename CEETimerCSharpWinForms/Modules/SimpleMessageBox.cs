f0b8f290dffd1f78670190fe5626686d28dd5e2e
WangHaonie <68315280+WangHaonie@users.noreply.github.com>
2024-04-19 21:28:06 +0800

    v3.0.2: 🛠️ 合并弹出消息框的代码减少重复

--- CEETimerCSharpWinForms/Modules/SimpleMessageBox.cs
+++ CEETimerCSharpWinForms/Modules/SimpleMessageBox.cs
@@ -10,65 +10,48 @@ namespace CEETimerCSharpWinForms.Modules
 
     public class MessageX
     {
-        public static void Popup(string Message, MessageLevel Level)
+        public static void Popup(string Message, MessageLevel Level, Form ParentForm = null, TabControl ParentTabControl = null, TabPage ParentTabPage = null)
         {
             var (Icon, Title) = GetIconTitle(Level);
-            MessageBox.Show(Message, Title, MessageBoxButtons.OK, Icon);
+            
+            if (ParentForm != null)
+            {
+                ParentForm.Invoke(new Action(() =>
+                {
+                    ParentForm.WindowState = FormWindowState.Normal;
+                    ParentForm.Activate();
+                    ParentTabControl.SelectedTab = ParentTabPage;
+                    MessageBox.Show(Message, Title, MessageBoxButtons.OK, Icon);
+                }));
+            }
+            else
+            {
+                MessageBox.Show(Message, Title, MessageBoxButtons.OK, Icon);
+            }
+        }
+
+        public static void Popup(string Message, Exception ex, Form ParentForm = null, TabControl ParentTabControl = null, TabPage ParentTabPage = null)
+        {
+            if (ParentForm != null)
+            {
+                ParentForm.Invoke(new Action(() =>
+                {
+                    ParentForm.WindowState = FormWindowState.Normal;
+                    ParentForm.Activate();
+                    ParentTabControl.SelectedTab = ParentTabPage;
+                    MessageBox.Show($"{Message}\n\n错误信息: \n{ex.Message}\n\n错误详情: \n{ex}", LaunchManager.ErrMsg, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }));
+            }
+            else
+            {
+                MessageBox.Show($"{Message}\n\n错误信息: \n{ex.Message}\n\n错误详情: \n{ex}", LaunchManager.ErrMsg, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         public static DialogResult Popup(string Message, MessageLevel Level, MessageBoxButtons Buttons)
         {
             var (Icon, Title) = GetIconTitle(Level);
             return MessageBox.Show(Message, Title, Buttons, Icon);
-        }
-
-        public static void Popup(string Message, MessageLevel Level, Form ParentForm)
-        {
-            var (Icon, Title) = GetIconTitle(Level);
-            ParentForm?.Invoke(new Action(() =>
-            {
-                ParentForm.WindowState = FormWindowState.Normal;
-                ParentForm.Activate();
-                MessageBox.Show(Message, Title, MessageBoxButtons.OK, Icon);
-            }));
-        }
-
-        public static void Popup(string Message, MessageLevel Level, Form ParentForm, TabControl TabControl, TabPage TabPage)
-        {
-            var (Icon, Title) = GetIconTitle(Level);
-            ParentForm?.Invoke(new Action(() =>
-            {
-                ParentForm.WindowState = FormWindowState.Normal;
-                ParentForm.Activate();
-                TabControl.SelectedTab = TabPage;
-                MessageBox.Show(Message, Title, MessageBoxButtons.OK, Icon);
-            }));
-        }
-
-        public static void Popup(string Message, Exception ex)
-        {
-            MessageBox.Show($"{Message}\n\n错误信息: \n{ex.Message}\n\n错误详情: \n{ex}", LaunchManager.ErrMsg, MessageBoxButtons.OK, MessageBoxIcon.Error);
-        }
-
-        public static void Popup(string Message, Exception ex, Form ParentForm)
-        {
-            ParentForm?.Invoke(new Action(() =>
-            {
-                ParentForm.WindowState = FormWindowState.Normal;
-                ParentForm.Activate();
-                MessageBox.Show($"{Message}\n\n错误信息: \n{ex.Message}\n\n错误详情: \n{ex}", LaunchManager.ErrMsg, MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }));
-        }
-
-        public static void Popup(string Message, Exception ex, Form ParentForm, TabControl TabControl, TabPage TabPage)
-        {
-            ParentForm?.Invoke(new Action(() =>
-            {
-                ParentForm.WindowState = FormWindowState.Normal;
-                ParentForm.Activate();
-                TabControl.SelectedTab = TabPage;
-                MessageBox.Show($"{Message}\n\n错误信息: \n{ex.Message}\n\n错误详情: \n{ex}", LaunchManager.ErrMsg, MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }));
         }
 
         private static (MessageBoxIcon, string) GetIconTitle(MessageLevel Level)
